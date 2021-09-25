aa9d07c2c40bc94d614503b544962f3fade28cf8
Alberto Candido <albertocandido98@gmail.com>
2021-09-25 13:09:20 -0300

    Adiciona testes de adiçao de tarefas e mudança de estado para finalizada

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -27,3 +27,58 @@ describe('Verifica se componentes necessários estão na tela', () => {
     });
   })
 });
+
+describe('Testa comportamentos de usuário', () => {
+
+  afterEach(() => {
+    localStorage.clear();
+  })
+
+  it('verifica adição de tarefa', () => {
+    const home = render(<App />);
+    const input = home.getByPlaceholderText('Add a task');
+    const button = home.getByText('Add task');
+
+    fireEvent.change(input, {target: {value: 'Teste'}});
+    expect(input.value).toBe('Teste');
+
+    fireEvent.click(button);
+    expect(input.value).toBe('');
+  });
+
+  it('verifica se tarefa foi adicionada', () => {
+    const home = render(<App />);
+    const input = home.getByPlaceholderText('Add a task');
+    const button = home.getByText('Add task');
+
+    fireEvent.change(input, {target: {value: 'Primeiro'}});
+    fireEvent.click(button);
+
+    fireEvent.change(input, {target: {value: 'Segundo'}});
+    fireEvent.keyPress(input, {key: 'Enter', code: 13, charCode: 13});
+
+    expect(home.getByText('Primeiro')).toBeInTheDocument();
+    expect(home.getByText('Segundo')).toBeInTheDocument();
+  });
+
+  it('verifica se tarefa foi adicionada e está marcada como finalizada e se pode ser desmarcada', () => {
+    const home = render(<App />);
+    const input = home.getByPlaceholderText('Add a task');
+    const button = home.getByText('Add task');
+    const finishButton = home.getByText('Finish / Unfinish');
+
+    fireEvent.change(input, {target: {value: 'Terceiro'}});
+    fireEvent.click(button);
+
+    const task = home.getByText('Terceiro');
+    fireEvent.click(task);
+    fireEvent.click(finishButton);
+    
+    expect(task.parentElement).toHaveClass('doneTask');
+
+    fireEvent.click(task);
+    fireEvent.click(finishButton);
+
+    expect(task.parentElement).not.toHaveClass('doneTask');
+  });
+});
